70e0d5ba6857cafb8a18edc8dbcba698ac4100e6
Anas123-svg <bscs23190@itu.edu.pk>
2025-07-14 01:08:06 +0500

    fix redis issue

--- app/services/data_fetcher.py
+++ app/services/data_fetcher.py
@@ -4,9 +4,10 @@ from app.utils.cache_utils import redis_get_df, redis_set_df
 async def fetch_data(symbol, date_range=''):
     if not symbol or not date_range:
         raise ValueError("Symbol and date range must be provided")
-    stock_key = f"{symbol}_{date_range}_data"
-    df = redis_get_df(stock_key)
-    if df is None:
-        df = get_stock_data(symbol, date_range)
-        redis_set_df(stock_key, df)
+    df = get_stock_data(symbol, date_range)
+    #stock_key = f"{symbol}_{date_range}_data"
+    #df = redis_get_df(stock_key)
+    #if df is None:
+     #   df = get_stock_data(symbol, date_range)
+      #  redis_set_df(stock_key, df)
     return df
